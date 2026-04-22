5f5da99df62b49ce3d0ce4fc68259eacfb7ed3b1
apichai <apichai@MacBook-Air-khxng-apichai.local>
2026-04-22 16:14:09 +0700

    Add CORS headers for DELETE

--- server.js
+++ server.js
@@ -12,6 +12,17 @@ let memoryResults = [];
 app.use(express.json());
 app.use(express.static(__dirname));
 
+// CORS headers
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.htm'));
 });
